e406d25b54b4b208f7a8b1af7d15acd1a8210c5c
Jasmeet Singh <mailtojasmeet@gmail.com>
2023-03-18 12:06:39 +0530

    tests added

--- myapp/tests.py
+++ myapp/tests.py
@@ -1,3 +1,22 @@
-from django.test import TestCase
+import unittest
+from unittest import mock
+from google.oauth2.credentials import Credentials
+from google.ads.google_ads.client import GoogleAdsClient
+from management.commands import google_ads
 
-# Create your tests here.
+
+class TestGoogleAds(unittest.TestCase):
+    @mock.patch('google.auth.default')
+    def test_get_google_ads_client(self, mock_default):
+        # Set up the mock to return a test credentials object
+        mock_credentials = mock.MagicMock(spec=Credentials)
+        mock_default.return_value = (mock_credentials, 'test-project-id')
+
+        # Call the function to create the Google Ads API client
+        client = google_ads.get_google_ads_client()
+
+        # Check that the client is an instance of GoogleAdsClient
+        self.assertIsInstance(client, GoogleAdsClient)
+
+        # Check that the client was created with the expected credentials
+        self.assertIs(client.credentials, mock_credentials)
